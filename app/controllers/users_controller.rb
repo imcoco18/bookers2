c8d111ca3e2eee49179a56f2169aeee7430f5259
imcoco18 <coco.jasmine18@icloud.com>
2020-04-25 23:47:10 +0900

    [再]課題

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+
+
+
+  def index
+  	@users = User.all
+  	@user = current_user
+  	@book = Book.new
+  end
+
+  def create
+  	@user = User.new(
+  		name: params[:name],
+  		email: params[:email],
+  		password: params[:password]
+  		)
+  	if @user.save
+  		flash[:notice] = "Signed in successfully."
+  		redirect_to users_pash(user.id)
+  	end
+  end
+
+  def show
+  	@book = Book.new
+  	@user = User.find(params[:id])
+  	@books = @user.books
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  	if @user.id != current_user.id
+  		redirect_to user_path(current_user)
+  	end
+  end
+
+  def update
+  	@user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render "edit"
+    end
+  end
+
+  private
+	def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+	end
+
+end
